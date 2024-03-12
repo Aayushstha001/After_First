391896113cd14f655d923a4f0dfe8f05e194221e
Aayushstha001 <153213991+Aayushstha001@users.noreply.github.com>
2024-03-12 21:47:52 +0545

    Update models.py

--- Co_lab/main/models.py
+++ Co_lab/main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Company(models.Model):
+    company_name = models.CharField(max_length=255, unique=True)
+    company_email = models.EmailField(max_length=255, unique=True)
+    company_telephone1 = models.CharField(max_length=20, unique=True)
+    company_telephone2 = models.CharField(max_length=20, unique=True)
+    company_registration_no = models.CharField(max_length=7, unique=True)
+    company_address = models.CharField(max_length=255, unique=True)
+    company_logo = models.ImageField(upload_to='company', null=True, blank=True)
+    company_description = models.TextField()
+    date_established = models.DateField()
+    created = models.DateTimeField(auto_now_add=True)
+    company_social_website = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.company_name
+    
+class Branch(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    branch_name = models.CharField(max_length=255)
+    branch_code = models.CharField(max_length=6, unique=True)
+
+    def __str__(self):
+        return self.branch_name
+    
+class Employee(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    branch_code = models.CharField(max_length=6, unique=True)
+    employee_code = models.CharField(max_length=8, unique=True)
+    phone_no = models.CharField(max_length=15, unique=True)
+    image = models.ImageField(upload_to='employee', null=True, blank=True)
+
+    def __str__(self):
+        return self.user
