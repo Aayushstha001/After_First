787693de7ab72f68c708deb3327f27cd3338a67c
Aayushstha001 <153213991+Aayushstha001@users.noreply.github.com>
2024-03-14 20:09:51 +0545

    yup

--- Co_lab/main/admin.py
+++ Co_lab/main/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import User
 
 admin.site.register(Company)
 admin.site.register(Branch)
 admin.site.register(Employee)
+
+class EmployeeInline(admin.StackedInline):
+    model = Employee
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = '__all__'
+    inlines = [EmployeeInline]
+
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
