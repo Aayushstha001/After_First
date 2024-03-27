e4c5216970a832c2033428c1f6d19ce0fc202247
Aayushstha001 <153213991+Aayushstha001@users.noreply.github.com>
2024-03-27 19:07:19 +0545

    pp

--- Co_lab/main/urls.py
+++ Co_lab/main/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     path('create-company-post/', views.create_company_post, name="create-company-post"),
     path('create-employee-post/', views.create_employee_post, name="create-employee-post"),
 
+    path('delete-company-post/<str:pk>/', views.delete_company_post, name="delete-company-post"),
+    path('delete-employee-post/<str:pk>/', views.delete_employee_post, name="delete-employee-post"),
+
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
     path('company_profile/<str:pk>/', views.companyProfile, name="company-profile"),
 ]
